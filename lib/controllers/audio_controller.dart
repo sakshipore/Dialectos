268023171e984c96ce9c6c64842617f5fcb731b4
sakshipore <88977771+sakshipore@users.noreply.github.com>
2023-03-30 16:45:49 +0530

    Accents list

---
+++ lib/controllers/audio_controller.dart
@@ -0,0 +1,78 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+
+class AudioController extends GetxController {
+  List<Map<String, dynamic>> dataList = [];
+  List<String> accentList = [];
+  bool isLoading = true;
+  // Future<String> getAudioFileDocumentId(String audioFileName) async {
+  //   try {
+  //     final querySnapshot = await FirebaseFirestore.instance
+  //         .collection('accents')
+  //         .where('name', isEqualTo: 'afrikaans')
+  //         .get();
+
+  //     final docSnapshot = querySnapshot.docs[0];
+  //     log("AUDIO FILE DOCUMENT ID $docSnapshot.id");
+  //     return docSnapshot.id;
+  //   } catch (e) {
+  //     log("AUDIO FILE DOCUMENT ID $e.toString()");
+  //     return "";
+  //   }
+  // }
+
+  // Future<String> getAudioFileUrl(String documentId) async {
+  //   try {
+  //     final docSnapshot = await FirebaseFirestore.instance
+  //         .collection('accents')
+  //         .doc(documentId)
+  //         .get();
+
+  //     final data = docSnapshot.data();
+  //     if (data != null && data['audioUrl'] != null) {
+  //       log("AUDIO FILE URL $data['audioUrl']");
+  //       return data['audioUrl'];
+  //     } else {
+  //       return "";
+  //     }
+  //   } catch (e) {
+  //     log("AUDIO FILE URL $e.toString()");
+  //     return "";
+  //   }
+  // }
+
+  Future<void> getAllDocuments() async {
+    await FirebaseFirestore.instance
+        .collection('accents')
+        .get()
+        .then((QuerySnapshot snapshot) {
+      for (var doc in snapshot.docs) {
+        var data = doc.data();
+        dataList.add(data as Map<String, dynamic>);
+      }
+    });
+
+    // log(dataList.toString());
+  }
+
+  Future<List<String>> getListOfAccents() async {
+    try {
+      log(dataList.length.toString());
+      accentList = dataList
+          .map((map) => map.keys.toList())
+          .toList()
+          .expand((list) => list)
+          .toList();
+
+      log(accentList.toString());
+      return accentList;
+    } catch (e) {
+      return [];
+    } finally {
+      isLoading = false;
+      update();
+    }
+  }
+}
--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,9 +1,36 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:dialectos/constants/text_style.dart';
+import 'package:dialectos/controllers/audio_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  AudioController audioController = Get.put(AudioController());
+  AudioPlayer audioPlayer = AudioPlayer();
+  String? documentId;
+  String? audioFileUrl;
+
+  @override
+  void initState() {
+    
+    gettingAudioFiles();
+    super.initState();
+  }
+
+  gettingAudioFiles() async {
+    await audioController.getAllDocuments();
+    await audioController.getListOfAccents();
+    // documentId = await audioController.getAudioFileDocumentId("afrikaans");
+    // audioFileUrl = await audioController.getAudioFileUrl(documentId!);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,58 +40,71 @@ class HomeScreen extends StatelessWidget {
         toolbarHeight: 50.h,
         elevation: 0,
       ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.w),
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 15.h,
-                ),
-                Row(
-                  children: [
-                    Icon(Icons.arrow_back_ios_new_rounded,
-                        color: Color(0xffC5E83A), size: 20.sp
-                        // size: 12.sp,
-                        ),
-                    SizedBox(
-                      width: 10.w,
+      body: GetBuilder<AudioController>(builder: (controller) {
+        return SafeArea(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.w),
+              child: controller.isLoading ? Center(child: CircularProgressIndicator()) :
+              Column(
+                children: [
+                  SizedBox(
+                    height: 15.h,
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.arrow_back_ios_new_rounded,
+                          color: Color(0xffC5E83A), size: 20.sp
+                          // size: 12.sp,
+                          ),
+                      SizedBox(
+                        width: 10.w,
+                      ),
+                      Text(
+                        "Back",
+                        style: MyTextStyle.normalTextStyle
+                            .copyWith(fontSize: 12.sp),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20.sp,
+                  ),
+                  Container(
+                    height: 550.h,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.r),
+                      border: Border.all(
+                        width: 1.w,
+                        color: Color(0xff2C2C2C),
+                      ),
                     ),
-                    Text(
-                      "Back",
-                      style:
-                          MyTextStyle.normalTextStyle.copyWith(fontSize: 12.sp),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 20.sp,
-                ),
-                Container(
-                  height: 550.h,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.r),
-                    border: Border.all(
-                      width: 1.w,
-                      color: Color(0xff2C2C2C),
+                    child: Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 5.w, vertical: 5.h),
+                      child: Text(
+                        "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.",
+                        style: MyTextStyle.normalTextStyle,
+                      ),
                     ),
                   ),
-                  child: Padding(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 5.w, vertical: 5.h),
-                    child: Text(
-                      "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.",
-                      style: MyTextStyle.normalTextStyle,
-                    ),
+                  ListView.builder(
+                    itemCount: controller.accentList.length,
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(controller.accentList[index]),
+                      );
+                    },
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      }),
     );
   }
 }
